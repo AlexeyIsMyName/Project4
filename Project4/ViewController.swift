4dfe9785a4c293bfe5ce3ff1e5918c33dec4861c
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-10-25 13:24:48 +0300

    add decidePolicyFor logic

--- Project4/ViewController.swift
+++ Project4/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController, WKNavigationDelegate {
     
     var webView: WKWebView!
     var progressView: UIProgressView!
+    var websites = ["apple.com", "hackingwithswift.com"]
     
     override func loadView() {
         webView = WKWebView()
@@ -46,7 +47,7 @@ class ViewController: UIViewController, WKNavigationDelegate {
                             options: .new,
                             context: nil)
         
-        let url = URL(string: "https://www.hackingwithswift.com")!
+        let url = URL(string: "https://" + websites[0])!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
     }
@@ -60,18 +61,33 @@ class ViewController: UIViewController, WKNavigationDelegate {
         }
     }
     
+    func webView(_ webView: WKWebView,
+                 decidePolicyFor navigationAction: WKNavigationAction,
+                 decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        let url = navigationAction.request.url
+        
+        if let host = url?.host {
+            for website in websites {
+                if host.contains(website) {
+                    decisionHandler(.allow)
+                    return
+                }
+            }
+        }
+        
+        decisionHandler(.cancel)
+    }
+    
     @objc func openTapped() {
         let ac = UIAlertController(title: "Open page…",
                                    message: nil,
                                    preferredStyle: .actionSheet)
         
-        ac.addAction(UIAlertAction(title: "apple.com",
-                                   style: .default,
-                                   handler: openPage))
-        
-        ac.addAction(UIAlertAction(title: "hackingwithswift.com",
-                                   style: .default,
-                                   handler: openPage))
+        for website in websites {
+            ac.addAction(UIAlertAction(title: website,
+                                       style: .default,
+                                       handler: openPage))
+        }
         
         ac.addAction(UIAlertAction(title: "Cancel",
                                    style: .cancel))
